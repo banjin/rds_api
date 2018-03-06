7c7839b25fdaef3019e233342b45be322f996b3b
songhm <songhm@qssec.com>
2018-03-06 13:07:19 +0800

    增加文档接口

--- rds_api/urls.py
+++ rds_api/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-# from rest_framework.documentation import include_docs_urls
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/', include('api.urls')),
-    # url(r'^docs/', include_docs_urls(title='My API title')),
+    url(r'^docs/', include_docs_urls(title='My API title')),
 
 ]
