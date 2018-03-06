17596281448600a7cf695a73a3c7a63c0b619330
songhm <songhm@qssec.com>
2018-03-06 13:01:36 +0800

    去掉文档接口

--- rds_api/urls.py
+++ rds_api/urls.py
@@ -18,8 +18,8 @@ from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/', include('api.urls')),
-    url(r'^docs/', include_docs_urls(title='My API title')),
+    # url(r'^docs/', include_docs_urls(title='My API title')),
 
 ]
